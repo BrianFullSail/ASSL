349e8ab731e618fdaf63931175fdd2e259329642
Brian Alvarado <balvarado@student.fullsail.edu>
2021-11-18 11:20:07 -0500

    got the questions and choices listing, working on validation

--- assl-app/src/pages/Quiz.js
+++ assl-app/src/pages/Quiz.js
@@ -1,14 +1,56 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Navigate } from 'react-router-dom'
 
 function Quiz(){
     const location = useLocation()
     const id = location.state.data
-    console.log(location.state.data)
-    console.log(id)
+    const question = id.Questions
+
+    let questions = question.map((element) => {
+        let choices = element.Choices.map((element) => {
+            
+            return <article key={element.choice} style={styles.article}><input type="radio" name="choice" value={element.choice}></input>
+            <label>{element.choice}</label></article>
+        })
+        return <article key={element.question}>
+            <h2>{element.question}</h2>
+            {choices}
+        </article>
+    })
+
     return(
-        <p>Hello there!</p>
+        <section style={styles.main}>
+            <h1>{id.name}</h1>
+            <form style={styles.form} name="myForm">
+                <section style={styles.sections}>{questions}</section>
+                <input type="submit" style={{margin: "5rem"}}/>
+            </form>
+        </section>
     )
 }
 
 export default Quiz
+
+const styles = {
+    main: {
+        display: "flex",
+        flexDirection: "column",
+        alignContent: "center"
+    },
+    sections: {
+        width: "50%",
+        marginLeft: "25%",
+        display: "flex",
+        flexDirection: "column",
+    },
+    form: {
+        width: "50%",
+        marginLeft: "25%",
+    },
+    article: {
+        width: "75%",
+        display: "flex",
+        alignContent: "start",
+        marginLeft: "25%"
+    }
+}
--- assl-app/src/pages/Quizzes.js
+++ assl-app/src/pages/Quizzes.js
@@ -26,9 +26,8 @@ function Quizzes() {
 
 
     // map through the quizzes and display each quiz
-    let quizzes = quizData.map((element,i) => {
-        console.log(element)
-        return <p style={styles.btn}  key={element.id}><Link style={styles.links} to="../Quiz" state={{data: element.id}}>{element.name}<br/></Link></p>
+    let quizzes = quizData.map((element) => {
+        return <p style={styles.btn}  key={element.id}><Link style={styles.links} to="../Quiz" state={{data: element}}>{element.name}<br/></Link></p>
     })
     
     return (
